490bbefdfd4cffad59333e282ac0ceab228d4473
Max Trussell <maxtrussell@gmail.com>
2020-05-03 00:03:29 -0700

    add dig program

--- lib/inv.lua
+++ lib/inv.lua
@@ -1,4 +1,10 @@
--- select a block with given id
+-- TODO: add blaze rods, bamboo, kelp, etc.
+FUEL = {
+	["minecraft:coal"]=80,
+	["minecraft:charcoal"]=80,
+}
+
+-- select a block with given name
 function selectByName(name)
 	for i = 1,16 do
 		item = turtle.getItemDetail(i)
@@ -7,4 +13,29 @@ function selectByName(name)
 			return true
 		end
 	end
+	return false
+end
+
+-- select the first item in inv from table
+function selectFromTable(table)
+	for i = 1,16 do
+		item = turtle.getItemDetail(i)
+		if item ~=nil and table[item.name] ~= nil then
+			turtle.select(i)
+			return true
+		end
+	end
+	return false
+end
+
+-- checks if every inventory slot has a block in it
+-- NOTE: does not care if any slots still have space remainign
+function isFull()
+	for i = 1,16 do
+		item = turtle.getItemDetail(i)
+		if item == nil then
+			return false
+		end
+	end
+	return true
 end
