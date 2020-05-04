377b8564d5327847db5ec2e4b7b4c623f7052de6
Max Trussell <maxtrussell@gmail.com>
2020-05-03 19:47:36 -0700

    bug fix?

--- lib/quarry.lua
+++ lib/quarry.lua
@@ -5,6 +5,7 @@ os.loadAPI("/lib/nav.lua")
 -- start bottom front left
 function quarry(width, depth, height)
 	local facingForward = true
+	nav.forceForward()
 	for i = 1,height do
 		for j = 1,width do
 			for k = 1,depth do
@@ -12,22 +13,28 @@ function quarry(width, depth, height)
 			end
 			-- just finished digging one row
 			-- reposition to dig second row
-			if facingForward then
-				turtle.turnRight()
-				nav.forceForward()
-				turtle.turnRight()
-			else
-				turtle.turnLeft()
-				nav.forceForward()
-				turtle.turnLeft()
+			if j ~= width then
+				-- don't reposition on last row
+				if facingForward then
+					turtle.turnRight()
+					nav.forceForward()
+					turtle.turnRight()
+				else
+					turtle.turnLeft()
+					nav.forceForward()
+					turtle.turnLeft()
+				end
+				facingForward = not facingForward
 			end
 			-- toggle every row
-			facingForward = not facingForward
 		end
 		-- reposition to start digging plane above
-		nav.forceUp()
-		turtle.turnLeft()
-		turtle.turnLeft()
+		if i ~= height then
+			nav.forceUp()
+			turtle.turnLeft()
+			turtle.turnLeft()
+			facingForward = not facingForward
+		end
 	end
 end
 
