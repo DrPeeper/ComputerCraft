da02d4db2ed488533944219f026504cf36d5e93b
DrPeeper <coydiego@knights.ucf.edu>
2020-05-07 21:34:50 +0000

    Add queue functionality

---
+++ lib/q.lua
@@ -0,0 +1,36 @@
+function q()
+	local queue = { 
+			["top"] = 0
+			["bottom"] = 1
+		}
+	return queue
+end
+
+function qIsEmpty(q)
+	if q["bottom"] > q["top"] then
+		return true
+	end
+	return false
+end
+
+function addQ(item,q)
+	q["top"] = q["top"] + 1
+	q[q["top"]] = item
+end
+
+function popQ(q)
+	if not qIsEmpty(q) then
+		item = q[q["bottom"]]
+		q["bottom"] = q["bottom"] + 1
+		return item
+	end
+end
+
+function printQ(q)
+	for i,v in pairs(q) do
+		if not i == "bottom" and not i == "top" then
+			print(i,v)
+		end
+	end
+end
+
