c35c59bd5a90fda15ac3b93750c568265752f41f
Maxwell Trussell <maxwell.trussell@salesforce.com>
2020-05-06 17:27:51 -0700

    clean up quarry function

--- lib/quarry.lua
+++ lib/quarry.lua
@@ -3,11 +3,11 @@ os.loadAPI("/lib/fuel.lua")
 os.loadAPI("/lib/nav.lua")
 
 VALUABLES = {
-    ["minecraft:iron_ore"] = true,
-    ["minecraft:gold_ore"] = true,
-    ["minecraft:diamond"] = true,
-    ["minecraft:emerald"] = true,
-    ["minecraft:lapis_lazuli"] = true,
+   ["minecraft:iron_ore"] = true,
+   ["minecraft:gold_ore"] = true,
+   ["minecraft:diamond"] = true,
+   ["minecraft:emerald"] = true,
+   ["minecraft:lapis_lazuli"] = true,
 }
 
 -- add any potential fuel source to VALUABLES
@@ -15,41 +15,48 @@ for k,v in pairs(fuel.FUEL) do VALUABLES[k] = v end
 
 -- start bottom front left
 function quarry(width, depth, height, valuables)
-    local valuables = valuables or false  -- default to false
-	local facingForward = true
-	nav.forceForward()
-	for i = 1,height do
-		for j = 1,width do
-			for k = 1,depth-1 do
-                if valuables and inv.isFull() then inv.dropAllExcept(VALUABLES) end
-				nav.forceForward()
-			end
-			-- just finished digging one row
-			-- reposition to dig second row
-			if j ~= width then
-				-- don't reposition on last row
-				if facingForward then
-					turtle.turnRight()
-                    if valuables and inv.isFull() then inv.dropAllExcept(VALUABLES) end
-					nav.forceForward()
-					turtle.turnRight()
-				else
-					turtle.turnLeft()
-                    if valuables and inv.isFull() then inv.dropAllExcept(VALUABLES) end
-					nav.forceForward()
-					turtle.turnLeft()
-				end
-				facingForward = not facingForward
-			end
-			-- toggle every row
-		end
-		-- reposition to start digging plane above
-		if i ~= height then
-            if valuables and inv.isFull() then inv.dropAllExcept(VALUABLES) end
-			nav.forceUp()
-			turtle.turnLeft()
-			turtle.turnLeft()
-		end
-	end
+   local valuables = valuables or false  -- default to false
+   local facingForward = true
+   nav.forceForward() -- start inside quarry zone
+   for i = 1,height do
+      for j = 1,width do
+	 for k = 1,depth-1 do
+	    manageInv(VALUABLES)
+	    nav.forceForward()
+	 end
+	 -- Just finished digging one row.
+	 -- Now, reposition to dig next row,
+	 -- but don't reposition after last pass
+	 if j ~= width then
+	    if facingForward then
+	       turtle.turnRight()
+	       manageInv(VALUABLES)
+	       nav.forceForward()
+	       turtle.turnRight()
+	    else
+	       turtle.turnLeft()
+	       manageInv(VALUABLES)
+	       nav.forceForward()
+	       turtle.turnLeft()
+	    end
+	    facingForward = not facingForward
+	 end
+      end
+      -- reposition to start digging plane above,
+      -- but don't reposition after last pass
+      if i ~= height then
+	 manageInv(VALUABLES)
+	 nav.forceUp()
+	 turtle.turnLeft()
+	 turtle.turnLeft()
+      end
+   end
 end
 
+-- drops all non valuable items and condenses inventory when full
+function manageInv(valuables)
+   if valuables and inv.isFull() then
+      inv.dropAllExcept(VALUABLES)
+      inv.restack()
+   end
+end
